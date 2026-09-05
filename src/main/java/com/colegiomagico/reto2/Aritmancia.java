6cb4ba923c203d7e411188ac8525dc525c717f8a
deodraus <jeronimodeossaabad@gmail.com>
2026-09-05 16:07:15 +0000

    feat: Reto 2 corregido exitosamente.

--- src/main/java/com/colegiomagico/reto2/Aritmancia.java
+++ src/main/java/com/colegiomagico/reto2/Aritmancia.java
@@ -4,7 +4,6 @@ package com.colegiomagico.reto2;
  * Completa el método sumar para que retorne la suma de a y b.
  */
 public class Aritmancia {
-
     public static int sumar(int a, int b) {
         return a + b;
     }
